f8137060a3025845749af439ebfb259220df1660
hafiz1379 <hafizrasa1379@gmail.com>
2023-12-30 15:26:12 +0430

    Add ShoeDetails component to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Nav from './components/Nav';
+import ShoeDetails from './components/ShoeDetails';
 
 function App() {
   return (
     <div className="p-10 xl:px-24">
       <Nav />
+      <ShoeDetails />
     </div>
   );
 }
