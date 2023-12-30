8f73eaa6028c05d3168a0eea7da40c551fa4165b
hafiz1379 <hafizrasa1379@gmail.com>
2023-12-30 15:26:22 +0430

    Add ShoeDetails component

---
+++ src/components/ShoeDetails.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable */
+import Nike1 from '../assets/n1-min.png';
+
+function ShoeDetails() {
+  return (
+    <div className="flex flex-col lg:flex-row-reverse space-y-4">
+      <div className="flex-1 ">
+        <img src={Nike1} alt="" />
+      </div>
+      <div className="flex-1 space-y-6">
+        <h1 className="text-5xl font-bold md:text-9xl ">Nike Air Max 270</h1>
+        <p className="font-medium md:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quod vel asperiores quo culpa ad recusandae quaerat nihil facere.</p>
+        <p className="text-3xl font-extrabold md:text-6xl">
+            100$
+        </p>
+        <div className="space-x-10">
+        <button className="h-14 w-44 bg-black text-white hover:bg-gray-800 active:bg-gray-500">Add to bag</button>
+        <a href="#" className="font-bold text-lg underline underline-offset-4">
+            View Details
+        </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ShoeDetails;
