3482c988e2fca24a441a08ef9d6c43eef12c7954
hafiz1379 <hafizrasa1379@gmail.com>
2023-12-31 14:48:18 +0430

    Add keyframes and animations to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0px)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        float: 'float 4s ease-in-out infinite',
+        fadeIn: 'fadeIn 1s ease-in-out',
+      },
+    },
   },
   plugins: [],
 };
