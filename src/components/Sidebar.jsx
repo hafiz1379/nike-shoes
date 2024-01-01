b0970392e0258125a8208027b0af40e45b10343c
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-01 16:35:36 +0430

    Add overflow-y-auto to Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@
 function Sidebar({ children, isOpen, onClickClose }) {
   return (
 <div>
-    <div className={`p-5 fixed right-0 z-50 top-0 w-full h-full md:w-[50%] lg:w-[35%] shadow-lg bg-white transform transition duration-300 ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
+    <div className={`overflow-y-auto p-5 fixed right-0 z-50 top-0 w-full h-full md:w-[50%] lg:w-[35%] shadow-lg bg-white transform transition duration-300 ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
         <button onClick={onClickClose} className="absolute top-4 right-4 p-2 text-black font-bold">X</button>
         {children}
     </div>
