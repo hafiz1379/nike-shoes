35ab93055e0148cb61c065dde0a527b77bfa0194
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-01 16:01:34 +0430

    Add sidebar component to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import Nav from './components/Nav';
 import { NewArrivalsSection } from './components/NewArrivalsSection';
 import ShoeDetails from './components/ShoeDetails';
+import Sidebar from './components/Sidebar';
 import { SHOE_LIST } from './constant';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
-      <Nav />
+      <Nav onClickShoppingBtn={() => setIsSidebarOpen(true)} />
       <ShoeDetails />
       <NewArrivalsSection items={SHOE_LIST} />
+      <Sidebar
+        isOpen={isSidebarOpen}
+        onClickClose={() => setIsSidebarOpen(false)}
+      >
+        Hello
+      </Sidebar>
     </div>
   );
 }
