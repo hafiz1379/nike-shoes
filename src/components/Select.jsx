477822c7c5b884b4f629d2f55100c40f8bc57246
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-03 16:26:05 +0430

    Refactor Select component to include className and defaultValue props

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable */
 
-function Select({ title, options }) {
+import { twMerge } from "tw-merge";
+
+function Select({ title, options, className, defaultValue }) {
   return (
     <div>
-      <select defaultValue="" className="border border-gray-500 p-4">
+      <select defaultValue={defaultValue || ""} className={twMerge(`border border-gray-500 p-4 w-24 ${className}`) }>
         <option value="" disabled hidden>{title}</option>
         {options.map((option) => (
           <option value={option} key={option}>
