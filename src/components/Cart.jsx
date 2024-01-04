bf64077e3ee77cce87c1612338661edf14660eb7
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-04 09:53:06 +0430

    Update Cart component to include onClickTrash prop

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,16 @@
 /* eslint-disable */
 import CartItem from "./CartItem";
 
-function Cart({ cartItems }) {
+function Cart({ cartItems, onClickTrash }) {
   return (
     <div>
-      <h2 className="text-4xl font-bold mb-5">Carts</h2>
+      <h2 className="text-4xl font-bold mb-5 dark:text-white">Carts</h2>
       <ul className="space-y-5">
-        {cartItems.map((cartItem) => <li key={cartItem.product.id}>
-            <CartItem item={cartItem}/>
-        </li>)}
+        {cartItems.map((cartItem) => (
+          <li key={cartItem.product.id}>
+            <CartItem item={cartItem} onClickTrash={onClickTrash} />
+          </li>
+        ))}
       </ul>
     </div>
   );
