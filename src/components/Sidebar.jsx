e56eef9b13af35d60ca4b52705c8cbc0d0e3e317
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-04 09:52:23 +0430

    Refactor Sidebar component styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,12 +2,22 @@
 
 function Sidebar({ children, isOpen, onClickClose }) {
   return (
-<div>
-    <div className={`overflow-y-auto p-5 fixed right-0 z-50 top-0 w-full h-full md:w-[50%] lg:w-[35%] shadow-lg bg-white transform transition duration-300 ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
-        <button onClick={onClickClose} className="absolute top-4 right-4 p-2 text-black font-bold">X</button>
+    <div>
+      <div
+        className={`overflow-y-auto p-5 fixed right-0 z-50 top-0 w-full h-full md:w-[50%] lg:w-[35%] shadow-lg bg-white transform transition duration-300
+    dark:bg-night ${isOpen ? "translate-x-0" : "translate-x-full"}`}
+      >
+        <button
+          onClick={onClickClose}
+          className="absolute top-4 right-4 p-2 text-black font-bold dark:text-white"
+        >
+          X
+        </button>
         {children}
-    </div>
-       {isOpen && <div className="fixed top-0 left-0 z-20 w-full h-full bg-black opacity-50" />}
+      </div>
+      {isOpen && (
+        <div className="fixed top-0 left-0 z-20 w-full h-full bg-black opacity-50" />
+      )}
     </div>
   );
 }
